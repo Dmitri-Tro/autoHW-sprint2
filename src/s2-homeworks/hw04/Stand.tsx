2e3c400e16d2fb205a28328b0253a50cdace95f8
Dmitri <dtroshchenkov@gmail.com>
2024-01-02 22:16:57 +0200

    hw4

--- src/s2-homeworks/hw04/Stand.tsx
+++ src/s2-homeworks/hw04/Stand.tsx
@@ -29,12 +29,8 @@ const Stand = () => {
                         onChangeText={setValue}
                         error={error}
                         onEnter={() => {
-                            setError(
-                                stateForAllInputs.trim()
-                                    ? ''
-                                    : 'Error'
-                            )
-                            setValue('')
+                            setError(stateForAllInputs.trim() ? '' : 'Error');
+                            setValue('');
                         }}
                     />
                 </div>
@@ -90,7 +86,7 @@ const Stand = () => {
                     <SuperCheckbox
                         id={'hw4-super-checkbox-like-old'}
                         checked={stateForAllCheckboxes}
-                        onChange={(e) => setChecked(e.currentTarget.checked)}
+                        onChange={e => setChecked(e.currentTarget.checked)}
                     />
                 </div>
             </div>
